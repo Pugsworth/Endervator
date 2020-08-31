5b040020780e44da97421387b4135c8f67de3500
Pugsworth <pworthwellington@gmail.com>
2020-08-30 20:38:49 -0600

    A couple changes

--- src/main/java/net/pugsworth/endervator/Block/EndervatorBlock.java
+++ src/main/java/net/pugsworth/endervator/Block/EndervatorBlock.java
@@ -1,27 +1,56 @@
 package net.pugsworth.endervator.Block;
 
+import java.util.Random;
+
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Block.Settings;
+import net.minecraft.client.particle.RedDustParticle;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateFactory;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.pugsworth.endervator.EndervatorMod;
 
-public class EndervatorBlock extends Block
+public class EndervatorBlock extends Block 
 {
+    public static int MAXIMUM_DISTANCE = 16;
+    public static BooleanProperty POWERED = Properties.POWERED;
+
     public EndervatorBlock(Settings settings)
     {
         super(settings);
+        this.setDefaultState((BlockState)(this.getDefaultState()).with(POWERED, false));
     }
 
-    public static int MAXIMUM_DISTANCE = 16;
+    @Override
+    protected void appendProperties(StateFactory.Builder<Block, BlockState> stateFactory)
+    {
+        stateFactory.add(POWERED);
+    }
+
+    public void neighborUpdate(BlockState blockState, World world, BlockPos blockPos, Block block, BlockPos blockPos2, boolean boolean_1)
+    {
+        if (!world.isClient)
+        {
+            boolean powered = world.isReceivingRedstonePower(blockPos);
+            world.setBlockState(blockPos, blockState.with(POWERED, powered), 3);
+        }
+     }
+
+    @Override
+    public int getLuminance(BlockState blockState_1) {
+        return blockState_1.get(POWERED) ? 0 : super.getLuminance(blockState_1);
+    }
 
     public static boolean isStandingOn(PlayerEntity entity, World world)
     {
@@ -37,6 +66,10 @@ public class EndervatorBlock extends Block
 
     public static BlockPos FindEndervator(World world, BlockPos origin, Direction direction)
     {
+        if (world.getBlockState(origin).get(POWERED)) {
+            return null;
+        }
+
         BlockPos newpos = origin;
         Block newblock;
         for (int i = 0; i < MAXIMUM_DISTANCE; i++)
@@ -58,6 +91,25 @@ public class EndervatorBlock extends Block
         entity.teleport((double)newpos.getX() + 0.5, (double)newpos.getY(), (double)newpos.getZ() + 0.5);
         world.playSound(null, newpos.getX(), newpos.getY(), newpos.getZ(), SoundEvents.ENTITY_SHULKER_TELEPORT, SoundCategory.BLOCKS, 1.0f, 1.0f);
         DoParticles(world, entity.getBlockPos(), newpos);
+    }
+
+    @Environment(EnvType.CLIENT)
+    public void randomDisplayTick(BlockState blockState, World world, BlockPos blockPos, Random random)
+    {
+        if (blockState.get(POWERED)) {
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            double rx = random.nextDouble() * 0.25;
+            double ry = random.nextDouble() * 0.25;
+            double rz = random.nextDouble() * 0.25;
+            double vx = ((double)random.nextFloat() - 0.5D) * 0.5D;
+            double vy = ((double)random.nextFloat() - 0.5D) * 0.5D;
+            double vz = ((double)random.nextFloat() - 0.5D) * 0.5D;
+
+            world.addParticle(ParticleTypes.PORTAL, blockPos.getX() + 0.5, blockPos.getY() + 0.5 + ry, blockPos.getZ() + 0.5, vx, vy, vz);
+        }
     }
 
     @Environment(EnvType.CLIENT)
--- src/main/java/net/pugsworth/endervator/EndervatorMod.java
+++ src/main/java/net/pugsworth/endervator/EndervatorMod.java
@@ -10,15 +10,17 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.pugsworth.endervator.Block.EndervatorBlock;
 
 public class EndervatorMod implements ModInitializer
 {
 
-	public static final Block ENDERVATOR_BLOCK = new Block(FabricBlockSettings
+	public static final Block ENDERVATOR_BLOCK = new EndervatorBlock(FabricBlockSettings
 		.of(Material.METAL)
 		.breakByTool(FabricToolTags.PICKAXES)
-		.lightLevel(5)
+		.lightLevel(6)
 		.strength(5, 6)
+		.ticksRandomly()
 		.build()
 	);
 
@@ -29,3 +31,11 @@ public class EndervatorMod implements ModInitializer
 		Registry.register(Registry.ITEM, new Identifier("endervator", "endervator_block"), new BlockItem(ENDERVATOR_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
 	}
 }
+
+
+/**
+ * TODO:
+ * 
+ * particles
+ * check space above endervator
+ */
